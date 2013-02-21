ff80be5bf3ec9f3bea220668b9815840c48a2fd6
Kristof Mattei <github@kristofmattei.be>
2013-02-21 17:41:30 +0100

    correct name

---
+++ WindowsService.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace WindowsService.Test
+{
+	using Service;
+
+	internal static class Program
+	{
+		private static void Main()
+		{
+			using (var monitor = new Monitor("c:\\foobar", "*.dll"))
+			{
+				monitor.Start();
+
+				// breakpoints in your actual service should not be hit :)
+			}
+		}
+	}
+}
