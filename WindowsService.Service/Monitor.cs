a2a3a4f990fd62c41ea7c10a2f8356378b2dcdc2
Kristof Mattei <github@kristofmattei.be>
2013-02-21 17:40:22 +0100

    Initial commit

---
+++ WindowsService.Service/Monitor.cs
@@ -0,0 +1,57 @@
+﻿namespace WindowsService.Service
+{
+	using System;
+	using System.IO;
+
+	public class Monitor : IDisposable
+	{
+		private FileSystemWatcher _fileSystemWatcher;
+
+		public Monitor(string path, string filter)
+		{
+			this._fileSystemWatcher = new FileSystemWatcher();
+
+			if (!Directory.Exists(path))
+			{
+				throw new DirectoryNotFoundException(string.Format("Directory not found: {0}", path));
+			}
+
+			this._fileSystemWatcher.Path = path;
+			this._fileSystemWatcher.Filter = filter;
+
+			this._fileSystemWatcher.Created += this.fileSystemWatcher_Created;
+		}
+
+		public void Start()
+		{
+			this._fileSystemWatcher.EnableRaisingEvents = true;
+		}
+
+		public void Stop()
+		{
+			this._fileSystemWatcher.EnableRaisingEvents = false;
+		}
+
+		private void fileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+		{
+			// do magic
+		}
+
+		public void Dispose()
+		{
+			this.Stop();
+
+			this._fileSystemWatcher = null;
+		}
+
+		public void Pause()
+		{
+			this.Stop();
+		}
+
+		public void Continue()
+		{
+			this.Start();
+		}
+	}
+}
---
+++ WindowsService/SuperUberService.cs
@@ -0,0 +1,41 @@
+﻿namespace WindowsService
+{
+	using System.ServiceProcess;
+	using Service;
+
+	public partial class SuperUberService : ServiceBase
+	{
+		private Monitor _monitor;
+
+		public SuperUberService()
+		{
+			this.InitializeComponent();
+		}
+
+		protected override void OnStart(string[] args)
+		{
+			this._monitor = new Monitor("c:\\foobar", "*.dll");
+			this._monitor.Start();
+		}
+
+		protected override void OnStop()
+		{
+			this._monitor.Stop();
+		}
+
+		protected override void OnPause()
+		{
+			this._monitor.Pause();
+		}
+
+		protected override void OnContinue()
+		{
+			this._monitor.Continue();
+		}
+
+		protected override void OnShutdown()
+		{
+			this._monitor.Dispose();
+		}
+	}
+}
